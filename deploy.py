66e70171d0de1ad0b63e3e12c52a57830995c41f
sanghun9634 <sanghun9634@gmail.com>
2024-08-26 17:31:48 +0900

    test: deploy 도커 옵션 변경

--- deploy.py
+++ deploy.py
@@ -2,27 +2,31 @@ import os
 
 from prefect import flow
 from prefect.deployments import DeploymentImage
+from prefect.infrastructure.docker import DockerContainer
 from prefect.client.schemas.schedules import CronSchedule
 
 from flow import hun_tick2min_flow
 
 if __name__ == "__main__":
+
+    docker_container = DockerContainer(
+        image="hun-tick2min:0.3.10",
+        networks=["team5"],
+        image_pull_policy="ALWAYS",
+        auto_remove=True,
+        env={
+            "KAFKA_URL": os.getenv("KAFKA_URL"),
+            "SPARK_URL": os.getenv("SPARK_URL"),
+            "TICK_TOPIC": os.getenv("TICK_TOPIC"),
+            "MIN_TOPIC": os.getenv("MIN_TOPIC"),
+        },
+    )
+
     hun_tick2min_flow.deploy(
         name="hun_tick2min_deploy",
         work_pool_name="docker-agent-pool",
         work_queue_name="docker-agent",
-        image=DeploymentImage(
-            name="hun-tick2min",
-            tag="0.3.9",
-            dockerfile="Dockerfile",
-            platform="linux/arm64",
-            buildargs={
-                       "KAFKA_URL": os.getenv("KAFKA_URL"),
-                       "SPARK_URL": os.getenv("SPARK_URL"),
-                       "TICK_TOPIC": os.getenv("TICK_TOPIC"),
-                       "MIN_TOPIC": os.getenv("MIN_TOPIC"),
-                       },
-        ),
+        infrastructure=docker_container,
         schedule=(CronSchedule(cron="0 8 * * 1-5", timezone="Asia/Seoul")),
         build=True,
     )
