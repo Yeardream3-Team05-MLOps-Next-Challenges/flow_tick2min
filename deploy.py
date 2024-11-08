e18025339f0378ad09b10394d0882c1157453bbb
sanghun9634 <sanghun9634@gmail.com>
2024-11-09 01:54:53 +0900

    feat: poetry 버전 컨테이너 적용

--- deploy.py
+++ deploy.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         work_queue_name="docker-agent",
         image=DeploymentImage(
             name="hun-tick2min",
-            tag="0.4.0",
+            tag=os.getenv("VERSION"),
             dockerfile="Dockerfile",
             platform="linux/arm64",
             buildargs={
